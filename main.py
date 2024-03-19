a5cdae91857bb0c6e229735d6707b48547621b7b
elizailtnere <149582165+elizailtnere@users.noreply.github.com>
2024-03-19 14:14:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-        return "Sveiki!"
+   return "Sveiki!"
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app.run(port = 5000 )
 
     
